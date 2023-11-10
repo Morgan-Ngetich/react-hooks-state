5e7d756566ac23722501e9bb8813c58cb615026e
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2023-11-11 01:33:57 +0300

    Coded along

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
+// useState will return an array that has two variables inside of it:
+
+// count: a reference to the current value of that state in React's internals
+// setCount: a setter function so we can update that state
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  //   const countState = useState(0);
+  // // => [0, setStateFunction]
+  // const count = countState[0];
+  // const setCount = countState[1];
+
+const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    // Update the value of count in React's internal state to be count + 1
+    // Re-render our component
+    setCount(count => count + 1);
+    setCount(count => count + 1);
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
